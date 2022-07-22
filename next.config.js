3132cb0548bb6ae3ecde90a097c1968d136ac1a6
Kirill Madorin <madorinkv@gmail.com>
2022-07-22 12:06:41 +0300

    next config updated with static paths

--- next.config.js
+++ next.config.js
@@ -2,22 +2,36 @@
 const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = {
-	experimental: { images: { layoutRaw: true } },
+	experimental: {images: {layoutRaw: true}},
 	...withAntdLess({
-	// optional: you can modify antd less variables directly here
-	// modifyVars: { '@primary-color': '#04f' },
-	// Or better still you can specify a path to a file
-	lessVarsFilePath: './src/styles/variables.less',
-	// optional
-	lessVarsFilePathAppendToEndOfContent: false,
-	// optional https://github.com/webpack-contrib/css-loader#object
-	cssLoaderOptions: {},
+		// optional: you can modify antd less variables directly here
+		// modifyVars: { '@primary-color': '#04f' },
+		// Or better still you can specify a path to a file
+		lessVarsFilePath: './src/styles/variables.less',
+		// optional
+		lessVarsFilePathAppendToEndOfContent: false,
+		// optional https://github.com/webpack-contrib/css-loader#object
+		cssLoaderOptions: {},
 
-	// Other Config Here...
+		// Other Config Here...
 
-	webpack(config) {
-		return config;
-	},
+		webpack(config) {
+			return config;
+		},
 
-	localIdentName: '[hash:2]',
-})};
+		localIdentName: '[hash:2]',
+
+		exportPathMap: async function (
+			defaultPathMap,
+			{dev, dir, outDir, distDir, buildId}
+		) {
+			return {
+				'/': {page: '/'},
+				'/404': {page: '/404'},
+				'/500': {page: '/500'},
+				'/u/kmadorin': {page: '/u', query: {username: 'kmadorin'}},
+				'/u/kmadorin1': {page: '/u', query: {username: 'kmadorin1'}},
+			}
+		},
+	})
+};
